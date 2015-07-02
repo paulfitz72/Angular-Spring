f3289eb3432e59d7cbdb13f096d4566142239a91
paulfitz72 <paulf88@hotmail.com>
2015-07-02 16:37:36 +0100

    Rejected commit: Default

---
+++ src/main/java/webframework/WebframeworkApplication.java
@@ -0,0 +1,12 @@
+package webframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebframeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebframeworkApplication.class, args);
+    }
+}
